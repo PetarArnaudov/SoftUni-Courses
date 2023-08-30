aae065b442e33aaed972c6ba14d5cf72ca770892
Petar Arnaudov <143598922+PetarArnaudov@users.noreply.github.com>
2023-08-30 17:47:21 +0300

    first

---
+++ 01.FirstStepsInProgramming/04.Inches toCentimeters/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _04.Inches_toCentimeters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double numberInches = double.Parse(Console.ReadLine());
+            double numberToCentimeters = numberInches * 2.54;
+
+            Console.WriteLine(numberToCentimeters);
+        }
+    }
+}
---
+++ 01.FirstStepsInProgramming/08.PetShop/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _08.PetShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dogFood = int.Parse(Console.ReadLine());
+            int catFood = int.Parse(Console.ReadLine());
+
+            double sum = (dogFood * 2.5) + (catFood * 4);
+
+
+            Console.WriteLine($"{sum} lv.");
+        }
+    }
+}
