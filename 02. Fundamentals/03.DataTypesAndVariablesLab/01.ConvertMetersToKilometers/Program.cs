0acf49fe81255f4dd73f61f8268791c521b37321
Petar Arnaudov <petar.h.arnaudov@gmail.com>
2023-08-31 09:22:38 +0300

    Added additional solved tasks

---
+++ 02. Fundamentals/03.DataTypesAndVariablesLab/01.ConvertMetersToKilometers/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _01.ConvertMetersToKilometers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double meters = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{meters / 1000:f2}");
+        }
+    }
+}
