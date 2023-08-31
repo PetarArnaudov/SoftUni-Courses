dad3c7341dfb901d7df4901870badfb17a8472d6
Petar Arnaudov <petar.h.arnaudov@gmail.com>
2023-08-31 09:40:33 +0300

    Added more tasks

---
+++ 02. Fundamentals/03.DataTypesAndVariablesLab/03.ExactSumOfRealNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _03.ExactSumOfRealNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbersCount = int.Parse(Console.ReadLine());
+            decimal sum = 0;
+
+            for (int i = 0; i < numbersCount; i++) 
+            {
+                decimal currentNumber = decimal.Parse(Console.ReadLine());
+                sum += currentNumber;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
